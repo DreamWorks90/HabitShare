652cb4762b54d1ce7bc5068696af70ebd25960f3
KSV-JEYAWIN <ksvjeyawin@gmail.com>
2023-09-15 17:00:52 +0530

    jeyawin design

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,336 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            systemOverlayStyle:
+                SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+            backgroundColor: Colors.white,
+            centerTitle: true,
+            title: Text(
+              'Add Habbit',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Color(0xff000000),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.black,
+                size: 30,
+              ),
+            ),
+          ),
+          body: Center(
+            child: Card(
+              color: Colors.white70,
+              elevation: 20,
+              shadowColor: Colors.grey,
+              margin: EdgeInsets.all(10),
+              child: SingleChildScrollView(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Habit Type',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Color(0xff1855f4),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 50, vertical: 15),
+                                backgroundColor: const Color(0xff20a901),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0))),
+                            onPressed: () {},
+                            child: Text(
+                              'Build',
+                              style: TextStyle(fontSize: 20),
+                            )),
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 50, vertical: 15),
+                                backgroundColor: const Color(0xffe80505),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0))),
+                            onPressed: () {},
+                            child: Text(
+                              'Quit',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            )),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      controller: controller,
+                      decoration: InputDecoration(
+                        labelText: 'Habit Name :',
+                        labelStyle: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xff1855f4),
+                            fontWeight: FontWeight.bold),
+                        hintText: 'ex: Walking',
+                        border: OutlineInputBorder(),
+                        suffixIcon: Icon(Icons.sports_martial_arts),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        labelText: 'Habit Description :',
+                        labelStyle: TextStyle(
+                            color: Color(0xff1855f4),
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      width: 380,
+                      height: 67,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Color(0xff1855f4)),
+                      child:
+                          TextButton(onPressed: () {}, child: DropdownList()),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      width: 380,
+                      height: 67,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Color(0xff1855f4)),
+                      child: TextButton(
+                          onPressed: () {}, child: TimeDropdownList()),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        labelText: 'Term :',
+                        labelStyle: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xff1855f4),
+                            fontWeight: FontWeight.bold),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'FINISH',
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class DropdownList extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _DropdownListState createState() => _DropdownListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _DropdownListState extends State<DropdownList> {
+  String selectedOption = 'None'; // Default selection
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          child: Row(
+            children: [
+              Container(
+                child: Center(
+                  child: Text(
+                    'Frequency :   ',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Container(
+                child: Text(
+                  ' ' + selectedOption,
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.yellowAccent,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              Container(
+                child: DropdownButton<String>(
+                  value: selectedOption,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedOption = newValue!;
+                    });
+                  },
+                  items: <String>[
+                    'None',
+                    'Daily',
+                    'Weekly',
+                    'Monthly',
+                    'Custom'
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
+    );
+  }
+}
+
+class TimeDropdownList extends StatefulWidget {
+  @override
+  _TimeDropdownListState createState() => _TimeDropdownListState();
+}
+
+class _TimeDropdownListState extends State<TimeDropdownList> {
+  String selectedOption = 'None'; // Default selection
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          child: Row(
+            children: [
+              Container(
+                child: Center(
+                  child: Text(
+                    'Time :            ',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Container(
+                child: Text(
+                  ' ' + selectedOption,
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.yellowAccent,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              Container(
+                child: DropdownButton<String>(
+                  value: selectedOption,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedOption = newValue!;
+                    });
+                  },
+                  items: <String>[
+                    'None',
+                    'Morning',
+                    'Evening',
+                    'Night',
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
