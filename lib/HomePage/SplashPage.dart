d745d15c63459bac799e6b110b8353ad89597ad3
kalaivanipksv <kalaivanipksv@gmail.com>
2023-09-22 22:00:29 +0530

    splashpage done

---
+++ lib/HomePage/SplashPage.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'package:HabitShare/Constants.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 4), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => homePageWidget));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: primaryColor,
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/image1.png',
+              height: 150,
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          ],
+        )));
+  }
+}
