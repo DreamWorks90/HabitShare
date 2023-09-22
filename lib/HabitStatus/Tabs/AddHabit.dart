a9599e0abdac74e0066a7a9042e8cba0b3702994
kalaivanipksv <kalaivanipksv@gmail.com>
2023-09-22 22:15:26 +0530

    tabs UI and navigation done

---
+++ lib/HabitStatus/Tabs/AddHabit.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AddHabits extends StatefulWidget {
+  const AddHabits({super.key});
+  @override
+  State<AddHabits> createState() => _AddHabitsState();
+}
+
+class _AddHabitsState extends State<AddHabits> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Center(
+        child: Text(
+          "Add Habits",
+          style: TextStyle(
+              fontSize: 25, fontWeight: FontWeight.w500, color: Colors.white),
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/HabitStatus/Tabs/Habits.dart
@@ -0,0 +1,43 @@
+import 'package:HabitShare/Constants.dart';
+import 'package:flutter/material.dart';
+
+class HabitsTab extends StatefulWidget {
+  const HabitsTab({super.key});
+
+  @override
+  State<HabitsTab> createState() => _HabitsTabState();
+}
+
+class _HabitsTabState extends State<HabitsTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: primaryColor,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => signInWidget));
+              },
+              icon: arrowBackIcon),
+          title: Center(
+            child: Text(
+              "Habits",
+              style: appbarTextStyle,
+            ),
+          )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Center(
+        child: Text('Habits Tab Content'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => addHabitsWidget));
+        },
+        backgroundColor: Colors.blue,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
