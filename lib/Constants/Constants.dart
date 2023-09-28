1c4ea471695d8bd4374e9e37955e374a1ef97368
KSV-JEYAWIN <ksvjeyawin@gmail.com>
2023-09-28 18:10:13 +0530

    Constants

---
+++ lib/Constants/Constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xff1855f4);
+const Icon arrowBackIcon = Icon(Icons.arrow_back);
+const TextStyle appbarTextStyle =
+    TextStyle(fontSize: 25, fontWeight: FontWeight.w500, color: Colors.white);
+const TextStyle buttonTextStyle =
+    TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: Colors.white);
