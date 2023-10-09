500b0b71bc3549c6e74787ed7a8c41208522774a
happypanda <quisitive22@gmail.com>
2023-10-08 22:08:18 -0700

    Adding friends changes

--- lib/features/friends/Friends.dart
+++ lib/features/friends/Friends.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:HabitShare/Constants.dart';
 
 class FriendsTab extends StatefulWidget {
   const FriendsTab({super.key});
@@ -8,11 +9,47 @@ class FriendsTab extends StatefulWidget {
 }
 
 class _FriendsTabState extends State<FriendsTab> {
+  bool areButtonsVisible = false;
+  void toggleButtonsVisibility() {
+    setState(() {
+      areButtonsVisible = !areButtonsVisible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Friends Tab Content'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: toggleButtonsVisibility,
+          ),
+        ],
+        title: const Center(
+          child: Text("Friends", style: appbarTextStyle),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Visibility(
+                visible: areButtonsVisible,
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text("Hello")
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
